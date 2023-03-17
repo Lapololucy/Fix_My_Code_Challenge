e87a3d4a5555627cfb3044790e7f746b7194aa21
Lapololucy <lapololucy@gmail.com>
2023-03-17 01:13:33 -0700

    fixed

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -1,23 +1,31 @@
-#!/usr/bin/node
-/*
-    Print a square with the character #
-    The size of the square must be the first argument 
-    of the program.
-*/
+###
+#
+#  Sort integer arguments (ascending)
+#
+###
 
+result = []
+ARGV.each do |arg|
+    # skip if not integer
+    next if arg !~ /^-?[0-9]+$/
 
-if (process.argv.length <= 2) {
-    process.stderr.write("Missing argument\n");
-    process.stderr.write("Usage: ./1-print_square.js <size>\n");
-    process.stderr.write("Example: ./1-print_square.js 8\n");
-    process.exit(1)
-}
+    # convert to integer
+    i_arg = arg.to_i
 
-size = parseInt(process.argv[2], 10)
+    # insert result at the right position
+    is_inserted = false
+    i = 0
+    l = result.size
+    while !is_inserted && i < l do
+        if result[i] < i_arg
+            i += 1
+        else
+            result.insert(i, i_arg)
+            is_inserted = true
+            break
+        end
+    end
+    result << i_arg if !is_inserted
+end
 
-for (let i = 0 ; i < size ; i ++) {
-    for (let j = 0 ; j < size ; j ++) {
-        process.stdout.write("#");
-    }
-    process.stdout.write("\n");
-}
+puts result
